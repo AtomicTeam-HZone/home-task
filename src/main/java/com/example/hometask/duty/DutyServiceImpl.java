62e975851b9a2ca5ba03fd230aa622f51eb6576a
Marcin <grzegorczykmarcin4@gmail.com>
2022-04-24 16:29:56 +0200

    Asigning user to DutyId

--- src/main/java/com/example/hometask/duty/DutyServiceImpl.java
+++ src/main/java/com/example/hometask/duty/DutyServiceImpl.java
@@ -4,39 +4,43 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class DutyServiceImpl implements DutyService {
-    private static List<Duty> dutyList = new ArrayList<>();
+
+    private static Map<Long, Duty> duties = new HashMap<>();
 
     static {
-        dutyList.add(new Duty(1L, "Cleaning", DutyState.INPROGRESS));
-        dutyList.add(new Duty(2L, "Ironing", DutyState.DONE));
-        dutyList.add(new Duty(3L, "Shopping", DutyState.TODO));
+
+        duties.put(1l, new Duty(1L, "Cleaning", DutyState.INPROGRESS));
+        duties.put(2l, new Duty(2L, "Ironing", DutyState.DONE));
+        duties.put(3l, new Duty(3L, "Shopping", DutyState.TODO));
     }
 
     @Override
     public List<Duty> findAll() {
-        return dutyList;
+        return new ArrayList<>(duties.values());
     }
 
     @Override
     public Duty getDuty(Long dutyId) {
-        Optional<Duty> maybeDuty = dutyList.stream().filter(d -> d.getId().equals(dutyId)).findFirst();
-        if(maybeDuty.isEmpty()){
+        Duty maybeDuty = duties.get(dutyId);
+
+        if(maybeDuty==null){
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Actor Not Found");
         }
 
-        return maybeDuty.get();
+        return maybeDuty;
     }
 
     @Override
     public void asignDutyToUser(Long dutyId, Long userId) {
         // TODO implement method body
+        Duty duty = getDuty(dutyId);
+        duty.asignUser(userId);
+        duties.put(dutyId, duty);
     }
 
 
