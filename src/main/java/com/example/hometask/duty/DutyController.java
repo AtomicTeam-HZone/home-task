3b5119e0479dc447728af5b1693da2eddf04227b
Marcin <grzegorczykmarcin4@gmail.com>
2022-04-24 12:48:52 +0200

    Added button to display each duty details and asigin user

--- src/main/java/com/example/hometask/duty/DutyController.java
+++ src/main/java/com/example/hometask/duty/DutyController.java
@@ -1,5 +1,6 @@
 package com.example.hometask.duty;
 
+import com.example.hometask.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,18 +15,19 @@ import java.util.List;
 public class DutyController {
 
     private final DutyService dutyService;
+    private final UserService userService;
 
     @GetMapping("/duties")
-    public String getAllDuties(Model model){
+    public String getAllDuties(Model model) {
         List<Duty> duties = dutyService.findAll();
         model.addAttribute("duties", duties);
         return "duties";
     }
 
     @GetMapping("/duties/{dutyId}")
-    public String dutyDetails(@PathVariable Long dutyId, Model model){
+    public String dutyDetails(@PathVariable Long dutyId, Model model) {
         Duty dutyDetails = dutyService.getDuty(dutyId);
-        //TODO: Pobrać użytkowników
+        model.addAttribute("users", userService.findAll());
         model.addAttribute("duty", dutyDetails);
         return "duty-details";
     }
